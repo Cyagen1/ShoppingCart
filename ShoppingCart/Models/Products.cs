828d0234603b5ced55b014bbd07266bb892eba15
Filip <filip.cosic91@gmail.com>
2023-03-10 00:34:56 +0100

    Add project files.

---
+++ ShoppingCart/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoppingCart.Models
+{
+    public static class Products
+    {
+        public static Dictionary<string, Product> Dictionary = new Dictionary<string, Product>
+        {
+            { "A", new Product("A", 50, 3, 130) },
+            { "B", new Product("B", 30, 2, 45) },
+            { "C", new Product("C", 20) },
+            { "D", new Product("D", 15) },
+        };
+    }
+}
